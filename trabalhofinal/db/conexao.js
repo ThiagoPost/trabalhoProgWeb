4f449fdc7156aa6669b2ef979583549e6b3e8c78
Thiago <thiago.post@universo.univates.br>
2024-06-23 19:13:54 -0300

    Funçoes de conexão com o banco

--- trabalhofinal/db/conexao.js
+++ trabalhofinal/db/conexao.js
@@ -1,8 +1,10 @@
-const { Poll } = require('pg');
-require ('dotenv').config();
+//não funciona
 
+const { Client } = require('pg');
+const dotenv = require("dotenv")
+dotenv.config()
 
-const pool = pool({
+const client  = new Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -10,18 +12,4 @@ const pool = pool({
     port: process.env.DB_PORT,
 });
 
-// Teste de conexão
-pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Erro ao adquirir cliente do pool', err.stack);
-    }
-    client.query('SELECT NOW()', (err, result) => {
-      release();
-      if (err) {
-        return console.error('Erro ao executar consulta', err.stack);
-      }
-      console.log('Conexão bem-sucedida:', result.rows);
-    });
-  });
-  
-module.exports = pool;
+module.exports = client;
